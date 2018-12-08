4fb04e15b4bacfb268e9d31638958d80535815a4
MUHAMMED NABEEL M <nabeelin1994@gmail.com>
2018-12-09 01:16:40 +0530

    Add files via upload
    Given the radius of the circle, write a program to print the circumference and area of the circle. Write functions to calculate area and circumference

---
+++ ass6.py
@@ -0,0 +1,14 @@
+
+# Function to print circle circumference and area
+
+def update(a):
+    circumference=2*3.14*a
+    area =3.14*a*a
+    print("Circumference of your circle  :",circumference)
+    print("Area of your circle  :", area)
+
+
+
+a=float(input("Enter Radius of Circle  : "))
+
+update(a)
